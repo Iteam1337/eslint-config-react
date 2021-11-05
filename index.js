4a93aeed6e364645df77808d036adfc94c90c292
Andreas Lundqvist <momentiris@gmail.com>
2021-11-05 14:45:52 +0100

    feat: add ts support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
+    ecmaVersion: 2020,
     requireConfigFile: false,
     babelOptions: {
       presets: ['@babel/preset-react'],
@@ -11,12 +12,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   env: {
     browser: true,
     es6: true,
     jest: true,
     mocha: true,
+    amd: true,
+    node: true,
   },
   settings: {
     react: {
@@ -27,9 +32,8 @@ module.exports = {
     'prettier/prettier': 'warn',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'no-unexpected-multiline': 'error',
     'no-var': 'error',
-    'prefer-const': 'error',
+    'prefer-const': 'warn',
     'space-before-blocks': ['error', 'always'],
     'no-unused-vars': [
       'warn',
@@ -40,4 +44,45 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint', 'prettier'],
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+      ],
+
+      rules: {
+        'prettier/prettier': 'warn',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-var': 'error',
+        'prefer-const': 'warn',
+        'space-before-blocks': ['error', 'always'],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            ignoreRestSiblings: true,
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 }
